333fa61ff5283fb8bab68f6994ba89a8495f8185
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-03-10 01:01:24 -0500

    defined all states

--- statePattern.cpp
+++ statePattern.cpp
@@ -3,9 +3,102 @@
  * working a system that moves between different states 
  * on different actions. This example implements a state machine for
  * a simple regular expression to show the idea: 
- * 	aab*a 
+ * 	ab+a 
  */
 
 #include <stdio.h>
+#include <string>
+#include "statePattern.h"
+
+using namespace std; 
+
+void State0::addNewCharacter(char newChar){
+  if(newChar == 'a'){
+    State::FA->goToState(State::FA->getState1());
+  }
+  else{
+    State::FA->goToState(State::FA->getRejectState());
+  }
+}
+
+bool State0::isValid(){
+  return false;
+}
 
 
+void State1::addNewCharacter(char newChar){
+  if(newChar == 'a'){
+    State::FA->goToState(State::FA->getState1());
+  }
+  else{
+    State::FA->goToState(State::FA->getRejectState());
+  }
+}
+
+bool State1::isValid(){
+  return false;
+}
+
+
+void State2::addNewCharacter(char newChar){
+  if(newChar == 'a'){
+    State::FA->goToState(State::FA->getState1());
+  }
+  else{
+    State::FA->goToState(State::FA->getRejectState());
+  }
+}
+
+bool State2::isValid(){
+  return false;
+}
+
+void State3::addNewCharacter(char newChar){
+  if(newChar == 'a'){
+    State::FA->goToState(State::FA->getState1());
+  }
+  else{
+    State::FA->goToState(State::FA->getRejectState());
+  }
+}
+
+bool State3::isValid(){
+  return false;
+}
+
+void RejectState::addNewCharacter(char newChar){
+  if(newChar == 'a'){
+    State::FA->goToState(State::FA->getState1());
+  }
+  else{
+    State::FA->goToState(State::FA->getRejectState());
+  }
+}
+
+bool RejectState::isValid(){
+  return false;
+}
+
+
+FiniteState::~FiniteState(){
+   delete state0;
+   delete state1;
+   delete state2;
+   delete state3;
+   delete rejectState;
+}
+
+FiniteState::FiniteState(){
+  state0 = new State0(this); 
+  state1 = new State1(this);
+  state2 = new State2(this);
+  state3 = new State3(this);
+  rejectState = new RejectState(this);
+  currentState = state0;
+}
+
+int main(){
+
+
+  return 0;
+}
---
+++ statePattern.h
@@ -0,0 +1,73 @@
+class FiniteState;
+
+class State{
+
+  public:
+    State(FiniteState *fa):FA(fa){};
+    virtual void addNewCharacter(char newChar) = 0;
+    virtual bool isValid() = 0;
+  protected:
+    FiniteState *FA;
+
+};
+
+class State0 : public State{
+
+  public: 
+    State0(FiniteState *fa):State(fa){};
+    void addNewCharacter(char newChar);
+    bool isValid();
+};
+
+class State1 : public State{
+
+  public: 
+    State1(FiniteState *fa):State(fa){};
+    void addNewCharacter(char newChar);
+    bool isValid();
+};
+
+class State2 : public State{
+
+  public: 
+    State2(FiniteState *fa):State(fa){};
+    void addNewCharacter(char newChar);
+    bool isValid();
+};
+
+class State3 : public State{
+
+  public: 
+    State3(FiniteState *fa):State(fa){};
+    void addNewCharacter(char newChar);
+    bool isValid();
+};
+
+class RejectState: public State{
+
+  public: 
+    RejectState(FiniteState *fa):State(fa){};
+    void addNewCharacter(char newChar);
+    bool isValid();
+};
+
+
+
+class FiniteState{
+  public:
+    FiniteState();
+    ~FiniteState();
+    void goToState(State *nextState) { currentState = nextState;};
+    bool isValid() {return currentState->isValid();};
+
+    State* getState0(){return state0;}; 
+    State* getState1(){return state1;};
+    State* getState2(){return state2;};
+    State* getState3(){return state3;};
+    State* getRejectState(){return rejectState;};
+
+  private:
+    State *state0, *state1, *state2, *state3, *rejectState, *currentState;
+};
+
+
