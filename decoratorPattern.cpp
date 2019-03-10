21157a8e0216488afbdb986076512ec20afdd90c
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-03-09 23:09:19 -0500

    New pattern: state pattern

---
+++ decoratorPattern.cpp
@@ -0,0 +1,7 @@
+/*
+ * Decorator pattern: 
+ *    
+ *
+ *
+ *
+ */
---
+++ statePattern.cpp
@@ -0,0 +1,11 @@
+/* State Pattern 
+ * A nice pattern to avoid using too many conditionals when
+ * working a system that moves between different states 
+ * on different actions. This example implements a state machine for
+ * a simple regular expression to show the idea: 
+ * 	aab*a 
+ */
+
+#include <stdio.h>
+
+
